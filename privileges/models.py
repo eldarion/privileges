0ae78f65050119d92d8191de2f6a154772bfe397
Patrick Altman <paltman@gmail.com>
2011-03-04 10:30:22 -0600

    added unicode method

--- privileges/models.py
+++ privileges/models.py
@@ -6,3 +6,6 @@ class Privilege(models.Model):
     label = models.CharField(max_length=75, db_index=True)
     verbose_name = models.CharField(max_length=255)
     description = models.TextField()
+    
+    def __unicode__(self):
+        return self.verbose_name
