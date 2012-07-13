789432b84f71335a92ae25d99e8db6458511512f
Patrick Altman <paltman@gmail.com>
2012-07-12 22:23:13 -0400

    Update setup.py

--- privileges/__init__.py
+++ privileges/__init__.py
@@ -1,23 +1,4 @@
-VERSION = (0, 1, 0, "a", 1) # following PEP 386
-DEV_N = 3
-POST_N = 0
-
-
-def build_version():
-    version = "%s.%s" % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = "%s.%s" % (version, VERSION[2])
-    if VERSION[3] != "f":
-        version = "%s%s%s" % (version, VERSION[3], VERSION[4])
-        if DEV_N:
-            version = "%s.dev%s" % (version, DEV_N)
-    elif POST_N > 0:
-        version = "%s.post%s" % (version, POST_N)
-    return version
-
-
-__version__ = build_version()
-
+__version__ = "0.1"
 
 
 handlers = set()
