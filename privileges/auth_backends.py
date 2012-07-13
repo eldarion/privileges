091a14a751543295843214d2cb98fa7ff1560f49
Patrick Altman <paltman@gmail.com>
2012-07-12 23:14:08 -0400

    Remove requirement on pinax.apps.account

--- privileges/auth_backends.py
+++ privileges/auth_backends.py
@@ -1,17 +1,13 @@
-from pinax.apps.account import auth_backends
-
-import privileges
+from privileges.registration import registry
 
 
-class AuthenticationBackend(auth_backends.AuthenticationBackend):
-    supports_object_permissions = False
-    supports_anonymous_user = True
+class PrivilegesBackend(object):
     
-    def has_perm(self, user, perm):
-        if user.is_anonymous():
+    def has_perm(self, user_obj, perm, obj=None):
+        if user_obj.is_anonymous():
             return False
         
-        if privileges.has_privilege(user, perm):
+        if registry.has_privilege(user_obj, perm):
             return True
         
-        return super(AuthenticationBackend, self).has_perm(user, perm)
+        return super(PrivilegesBackend, self).has_perm(user_obj, perm)
