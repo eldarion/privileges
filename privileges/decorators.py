8a2fde50ab5b4664078210fe7a609de48123e294
Patrick Altman <paltman@gmail.com>
2012-07-12 23:08:22 -0400

    PEP8 Compliance
    I ignore W293 because I disagree with it.

--- privileges/decorators.py
+++ privileges/decorators.py
@@ -4,16 +4,28 @@ from django.contrib.auth import REDIRECT_FIELD_NAME
 from privileges import has_privilege
 
 
-def privilege_required(privilege, function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
-    """
-    Decorator for views that checks that the user has the right privileges,
-    redirecting to the log-in page if necessary.
-    """
+def privilege_required(privilege,
+                       function=None,
+                       redirect_field_name=None,
+                       login_url=None):
+    
+    if redirect_field_name is None:
+        redirect_field_name = REDIRECT_FIELD_NAME
+    
+    def test(user):
+        if user.is_authenticated() and has_privilege(user, privilege):
+            return True
+        if user.is_superuser:
+            return True
+        return False
+    
     actual_decorator = user_passes_test(
-        lambda u: (u.is_authenticated() and has_privilege(u, privilege)) or u.is_superuser,
+        test,
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )
+    
     if function:
         return actual_decorator(function)
+    
     return actual_decorator
