a0a7e92d4661b5fb3145e0210f71e6332179f387
Patrick Altman <paltman@gmail.com>
2012-07-17 12:58:21 -0500

    Remove first attempt at function based views

--- privileges/urls.py
+++ privileges/urls.py
@@ -2,11 +2,7 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns(
-    "privileges.views",
-    url(r"^(?P<username>\w+)/$", "grant_list",
         name="privileges_grant_list"
     ),
-    url(r"^(?P<username>\w+)/(?P<pk>\d+)/$", "grant_detail",
-        name="privileges_grant_detail"
     )
 )
--- privileges/views.py
+++ privileges/views.py
@@ -1,46 +1,14 @@
-from django.http import HttpResponseForbidden, Http404
-from django.shortcuts import render_to_response, get_object_or_404, redirect
-from django.template import RequestContext
 
-from django.contrib.auth.models import User
 
 from privileges.forms import GrantForm
 from privileges.models import Grant
 
 
-def grant_list(request, username):
     
-    user = get_object_or_404(User, username=username)
-    if request.user != user and not request.user.is_superuser:
-        return HttpResponseForbidden()
     
-    if request.method == "POST":
-        form = GrantForm(request.POST, user=request.user)
-        if form.is_valid():
-            form.save()
-            redirect("privileges_grant_list", username=username)
-    else:
-        form = GrantForm(user=request.user)
     
-    return render_to_response("privileges/grant_list.html", {
-        "grants_given": Grant.objects.filter(grantor=user),
-        "grants_received": Grant.objects.filter(grantee=user),
-        "grant_user": user,
-        "form": form
-    }, context_instance=RequestContext(request))
 
 
-def grant_detail(request, username, pk):
     
-    user = get_object_or_404(User, username=username)
-    if request.user != user and not request.user.is_superuser:
-        return HttpResponseForbidden()
     
-    grant = get_object_or_404(Grant, pk=pk)
-    if grant.grantor != user and grant.grantee != user:
-        return Http404()
     
-    return render_to_response("privileges/grant_detail.html", {
-        "grant": grant,
-        "grant_user": user
-    }, context_instance=RequestContext(request))
